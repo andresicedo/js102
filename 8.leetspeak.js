8d0b2a57dd191358dabb747c750c9ac8668cde07
Andres Icedo <andresicedo24@gmail.com>
2021-02-04 21:14:31 -0600

    problem 8

---
+++ 8.leetspeak.js
@@ -0,0 +1,40 @@
+/*
+## Leetspeak
+
+Write a function leetspeak which is given a string, and returns 
+the leetspeak equivalent of the string. To convert text to its 
+leetspeak version, make the following substitutions:
+
+> A => 4
+> E => 3
+> G => 6
+> I => 1
+> O => 0
+> S => 5
+> T => 7
+
+```js
+leetspeak('Leet');
+```
+l337
+```
+*/
+const subs = {
+    'A': 4,
+    'E': 3,
+    'G': 6,
+    'I': 1,
+    'O': 0,
+    'S': 5,
+    'T': 7
+}
+function leetspeak(string) {
+    //create an empty result string
+    let resutl = '';
+    //iterate over the string given
+    for(let i = 0; i < string.length; i++) {
+        
+    }
+ }
+
+leetspeak('Leet');
